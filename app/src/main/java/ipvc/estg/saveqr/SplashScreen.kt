13409a7f2f5f175f19cfcd21776f285a58d9180f
SergioRCarvalho <sergio_pvz3@hotmail.com>
2021-06-09 19:59:05 +0100

    SplashScreen
    SplashScreen que verifica a ligação à internet e tem um atalho para digitalizar qrcode

--- app/src/main/java/ipvc/estg/saveqr/SplashScreen.kt
+++ app/src/main/java/ipvc/estg/saveqr/SplashScreen.kt
@@ -1,44 +1,46 @@
 package ipvc.estg.saveqr
 
+import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import ipvc.estg.saveqr.ui.registar.RegistarFragment
 
 class SplashScreen : AppCompatActivity() {
 
-    private val SPLASH_TIME: Long = 3000
 
+    private val SPLASH_TIME: Long = 1500
+    private var registo=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        //   val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        // val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+           val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val qr = findViewById<ImageView>(R.id.qr)
-        val notConnected = intent.getBooleanExtra(
-            ConnectivityManager
-                .EXTRA_NO_CONNECTIVITY, false
-        )
-        if (notConnected != true) {
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+        if (isConnected == true) {
 
             Handler().postDelayed({
+                if (registo==0)
                 startActivity(Intent(this, MainActivity::class.java))
+                // close this activity
                 finish()
-            }, SPLASH_TIME)
 
+            }, SPLASH_TIME)
+            qr.setOnClickListener {
+                val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                intent.putExtra("EXTRA", "Registar")
+                startActivity(intent)
+                registo=1;
+            }
         } else {
             Toast.makeText(applicationContext, getString(R.string.s_conx_int), Toast.LENGTH_SHORT)
                 .show()
         }
-        qr.setOnClickListener {
-            val intent = Intent(this@SplashScreen, MainActivity::class.java)
-            intent.putExtra("EXTRA", "Registar")
-            startActivity(intent)
-// Now start your activity
-        }
+
     }
 }
