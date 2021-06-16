e6361566ff3e18140ad9d1f7f311f2c0a6df1c20
Joel Silva <henriquesa@ipvc.pt>
2021-06-16 23:52:47 +0100

    rebase

--- app/src/main/java/ipvc/estg/saveqr/MainActivity.kt
+++ app/src/main/java/ipvc/estg/saveqr/MainActivity.kt
@@ -2,7 +2,9 @@ package ipvc.estg.saveqr
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -12,10 +14,15 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.fragment_login.*
+import ipvc.estg.saveqr.api.ServiceBuilder
+import ipvc.estg.saveqr.api.endpoints.usersEndpoint
+import ipvc.estg.saveqr.api.models.Users
+import ipvc.estg.saveqr.api.models.UsersReturn
+import ipvc.estg.saveqr.ui.listapasta.ListaPastaFragment
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,17 +34,10 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+//12131
 
-        imageView.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.nav_listapasta, null))
 
 
 
@@ -51,13 +51,31 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         val frag = intent.getStringExtra("EXTRA")
-        if (frag=="Registar")
-        {
-           navController.navigate(R.id.nav_registar);
+        if (frag == "Listar") {
+            navController.navigate(R.id.nav_listapasta);
         }
+
+        val request = ServiceBuilder.buildService(usersEndpoint::class.java)
+        val call = request.getUsers()
+
+        call.enqueue(object : Callback<UsersReturn> {
+            override fun onResponse(call: Call<UsersReturn>, response: Response<UsersReturn>) {
+                //Toast.makeText(applicationContext, response.body()!!.msg, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call<UsersReturn>, t: Throwable) {
+                Toast.makeText(applicationContext, "DEU ERRO", Toast.LENGTH_LONG).show()
+                Log.d("ENDPONT", t.toString())
+            }
+        })
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+
+
+
+override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
@@ -66,5 +84,7 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    override fun onBackPressed() {
     }
 }
