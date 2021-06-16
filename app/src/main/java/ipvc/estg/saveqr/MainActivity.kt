ca37f91e1dd64cad61bed333fa61b93a02f06006
Joel Silva <henriquesa@ipvc.pt>
2021-06-16 23:31:34 +0100

    falta passar dados para menu drawer

--- app/src/main/java/ipvc/estg/saveqr/MainActivity.kt
+++ app/src/main/java/ipvc/estg/saveqr/MainActivity.kt
@@ -34,15 +34,7 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
 
-            //   val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            //       .requestEmail()
-        //      .build()
-
-        // Build a GoogleSignInClient with the options specified by gso.
-        //    val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
 
 
