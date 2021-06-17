b5dbfeb2d912b01d9e0a7c141a4072de246b9085
SergioRCarvalho <sergio_pvz3@hotmail.com>
2021-06-17 02:19:50 +0100

    LerQr_e_guardarQr

--- app/src/main/java/ipvc/estg/saveqr/ui/lerQr/LerQrActivity.kt
+++ app/src/main/java/ipvc/estg/saveqr/ui/lerQr/LerQrActivity.kt
@@ -1,19 +1,33 @@
 package ipvc.estg.saveqr.ui.lerQr
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.zxing.Result
 import ipvc.estg.saveqr.History
+import ipvc.estg.saveqr.MainActivity
 import ipvc.estg.saveqr.R
+import ipvc.estg.saveqr.api.ServiceBuilder
+import ipvc.estg.saveqr.api.endpoints.QrCodesEndpoint
+import ipvc.estg.saveqr.api.models.Qrcodes
 import ipvc.estg.saveqr.mvp.BaseMvpActivity
 import ipvc.estg.saveqr.ui.LerQr.LerQrActivityContract
 import ipvc.estg.saveqr.ui.data.ORM.HistoryORM
 import kotlinx.android.synthetic.main.activity_ler_qr.*
 import kotlinx.android.synthetic.main.dialog_scan_success.view.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.DateFormat
 import java.util.*
 
@@ -30,6 +44,9 @@ class LerQrActivity : BaseMvpActivity<LerQrActivityContract.View, LerQrActivityC
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ler_qr)
+
+        //Verificar permiss
+        checkPermission(Manifest.permission.CAMERA,401)
 
        mHistoryOrm = HistoryORM()
         initUI()
@@ -85,7 +102,40 @@ class LerQrActivity : BaseMvpActivity<LerQrActivityContract.View, LerQrActivityC
             result?.text.toString())
         mHistoryOrm?.add(this, history)
         mPresenter.qrCodeScanned(history)
+        val intent = Intent(this@LerQrActivity, MainActivity::class.java)
+        val request = ServiceBuilder.buildService(QrCodesEndpoint::class.java)
+        val call = request.postQrCode(
+            "qr exemplo",
+            result?.text.toString(),
+            5,
+            5
+        )
+        call.enqueue(object : Callback<Qrcodes> {
+            override fun onResponse(
+                call: Call<Qrcodes>,
+                response: Response<Qrcodes>
+            ) {
 
+
+                if (response.isSuccessful) {
+                    // Log.d("***","Sucesso")
+                    Toast.makeText(this@LerQrActivity, "Sucesso!", Toast.LENGTH_SHORT)
+                        .show();
+                    startActivity(intent)
+                    finish()
+                    // communicator.passDataconn(root.nome.text.toString())
+                    //  communicator.passDataconn(id,root.nome.text.toString(),root.username.text.toString(),root.email.text.toString(),root.password.text.toString())
+                    // getActivity()?.getSupportFragmentManager()?.beginTransaction().remove(this@RegistarFragment).commit();
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<Qrcodes>, t: Throwable) {
+                Toast.makeText(this@LerQrActivity, "Erro, tente mais tarde!", Toast.LENGTH_SHORT).show();
+            }
+        })
     }
 
     override fun showSuccessScanningDialog(result: String) {
@@ -112,5 +162,17 @@ class LerQrActivity : BaseMvpActivity<LerQrActivityContract.View, LerQrActivityC
     override fun continueScanning() {
         dialog?.dismiss()
         mScannerView?.resumeCameraPreview(this)
+    }
+
+    //Pedir premissao camara
+    //https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@LerQrActivity, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+           // Toast.makeText(this@LerQrActivity, "Without Permission", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this@LerQrActivity, arrayOf(permission), requestCode)
+        } else {
+          //  Toast.makeText(this@LerQrActivity, "Permission already granted", Toast.LENGTH_SHORT).show()
+        }
     }
 }
