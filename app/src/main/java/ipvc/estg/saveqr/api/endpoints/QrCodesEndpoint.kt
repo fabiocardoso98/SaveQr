c8b65833cd3d3a0da243650808263622434d9552
SergioRCarvalho <sergio_pvz3@hotmail.com>
2021-06-17 16:41:34 +0100

    atualizacao
    +api

---
+++ app/src/main/java/ipvc/estg/saveqr/api/endpoints/QrCodesEndpoint.kt
@@ -0,0 +1,24 @@
+package ipvc.estg.saveqr.api.endpoints
+
+import ipvc.estg.saveqr.api.models.QrCodesRegisterReturn
+import ipvc.estg.saveqr.api.models.QrCodesReturn
+import ipvc.estg.saveqr.api.models.Qrcodes
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface QrCodesEndpoint {
+    @GET("/qrcodes")
+    fun getUsers(): Call<QrCodesRegisterReturn>
+
+    @FormUrlEncoded
+    @POST("/qrcodes")
+    fun postQrCode(
+        @Field("name") name: String?,
+        @Field("content") content: String?,
+        @Field("categoryId") categoryId: Int?,
+        @Field("userId") userId: Int?
+    ): Call<Qrcodes>
+}
