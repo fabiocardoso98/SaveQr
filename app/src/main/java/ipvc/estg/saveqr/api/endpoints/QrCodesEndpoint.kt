06c10c3d546df5c54894cbf046d6fec2ab88181b
SergioRCarvalho <sergio_pvz3@hotmail.com>
2021-06-17 19:20:11 +0100

    atualizacao
    listaqr

--- app/src/main/java/ipvc/estg/saveqr/api/endpoints/QrCodesEndpoint.kt
+++ app/src/main/java/ipvc/estg/saveqr/api/endpoints/QrCodesEndpoint.kt
@@ -11,7 +11,7 @@ import retrofit2.http.POST
 
 interface QrCodesEndpoint {
     @GET("/qrcodes")
-    fun getUsers(): Call<QrCodesRegisterReturn>
+    fun getQrcodes(): Call<QrCodesRegisterReturn>
 
     @FormUrlEncoded
     @POST("/qrcodes")
