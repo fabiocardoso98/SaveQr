de792507bbf17d07c1067e8349588d6f9d3c6757
Joel Silva <henriquesa@ipvc.pt>
2021-07-12 02:16:43 +0100

    Falta endpoint + não entra no fragm UpdateQr

--- app/src/main/java/ipvc/estg/saveqr/ui/addqrupdate/AddQrUpdate.kt
+++ app/src/main/java/ipvc/estg/saveqr/ui/addqrupdate/AddQrUpdate.kt
@@ -1,19 +1,42 @@
 package ipvc.estg.saveqr.ui.addqrupdate
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.findNavController
 
 import ipvc.estg.saveqr.R
+import ipvc.estg.saveqr.api.ServiceBuilder
+import ipvc.estg.saveqr.api.api.endpoints.foldersEndpoint
+import ipvc.estg.saveqr.api.api.models.FoldersReturn
+import ipvc.estg.saveqr.api.models.Qrcodes
 import ipvc.estg.saveqr.ui.addqrupdate.AddQrUpdateViewModel
+import kotlinx.android.synthetic.main.add_qr_update_fragment.*
+import kotlinx.android.synthetic.main.popup_addpasta.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddQrUpdate : Fragment() {
 //pena
 
 //pena3
+
+    private lateinit var nomeQR: EditText
+    private lateinit var tipoQR: EditText
+    private lateinit var latlngQR: EditText
+    var allPastasLiveData = MutableLiveData<List<Qrcodes?>>()
+
     companion object {
         fun newInstance() = AddQrUpdate()
     }
@@ -24,6 +47,23 @@ class AddQrUpdate : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        var value = arguments?.getInt("id")
+
+
+
+        val name: String? = arguments?.getString("name").toString()
+        val tipo: String? = arguments?.getString("tipoId").toString()
+        val latlng: String? = arguments?.getString("latlng").toString()
+        nomeQR.setText(name)
+        tipoQR.setText(tipo)
+        latlngQR.setText(latlng)
+
+        addpasta.setOnClickListener {
+            updateQR()
+        }
+
+
         return inflater.inflate(R.layout.add_qr_update_fragment, container, false)
     }
 
@@ -31,6 +71,39 @@ class AddQrUpdate : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(AddQrUpdateViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun updateQR() {
+        nomeQR = nome
+
+        val id: Int? =  0
+
+
+        if (!TextUtils.isEmpty(nomeQR.text) || !TextUtils.isEmpty(tipoQR.text) || !TextUtils.isEmpty(latlngQR.text) ) {
+            val request = ServiceBuilder.buildService(foldersEndpoint::class.java)
+
+           // val call = request.setUpdateFolders(id!!,nomeQR.text.toString())
+
+            /*call.enqueue(object  : Callback<FoldersReturn> {
+                override fun onResponse(call: Call<FoldersReturn>, response: Response<FoldersReturn>) {
+                    if(response.isSuccessful) {
+
+
+
+
+                    }
+                }
+
+                override fun onFailure(call: Call<FoldersReturn>, t: Throwable) {
+
+                }
+            })*/
+
+        } else {
+            Toast.makeText(context, "Campos vazios", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 
 }
--- app/src/main/java/ipvc/estg/saveqr/ui/listaqr/ListaQrFragment.kt
+++ app/src/main/java/ipvc/estg/saveqr/ui/listaqr/ListaQrFragment.kt
@@ -1,5 +1,6 @@
 package ipvc.estg.saveqr.ui.listaqr
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,17 +10,25 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ipvc.estg.saveqr.R
+import ipvc.estg.saveqr.Swipes.SwipeToEditCallback
 import ipvc.estg.saveqr.api.ServiceBuilder
 import ipvc.estg.saveqr.api.api.endpoints.QrCodesEndpoint
+import ipvc.estg.saveqr.api.api.models.Folders
+import ipvc.estg.saveqr.api.api.models.FoldersReturn
 import ipvc.estg.saveqr.api.models.QrCodesReturn
 import ipvc.estg.saveqr.api.models.Qrcodes
+import ipvc.estg.saveqr.popup_insertPasta
+import ipvc.estg.saveqr.ui.addqrupdate.AddQrUpdate
 import ipvc.estg.saveqr.ui.listaqr.adapter.QrAdapter
 import retrofit2.Call
 import retrofit2.Callback
@@ -30,6 +39,9 @@ class ListaQrFragment : Fragment() {
 
 
     private lateinit var ListaQRviewModel: ListaQRViewModel
+    var allPastasLiveData = MutableLiveData<List<Qrcodes?>>()
+    var idpasta: Int = 0
+
 
 
     override fun onCreateView(
@@ -91,6 +103,60 @@ class ListaQrFragment : Fragment() {
                 Toast.makeText( activity, "DEU ERRO", Toast.LENGTH_LONG).show()
             }
         })
+
+        val swipeHandlerEdit = object : SwipeToEditCallback(requireContext()) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position: Int = viewHolder.adapterPosition
+                val id: Int = allPastasLiveData.value?.get(position)?.id ?: 0
+                val pastaTemp: Qrcodes? = allPastasLiveData.value!![position]
+                val bundle = bundleOf("id" to pastaTemp!!.id,"name" to pastaTemp!!.name, "latlng" to pastaTemp!!.latlng, "tipoId" to pastaTemp!!.categoryId )
+                findNavController().navigate(R.id.nav_addQrUpdate,bundle)
+                allPastasLiveData.value = allPastasLiveData.value!!.toMutableList().apply {
+                    removeAt(position)
+
+                   /* val call = request.getPastaByUser(idUser)
+                    call.enqueue(object : Callback<FoldersReturn> {
+                        override fun onResponse(
+                            call: Call<FoldersReturn>,
+                            response: Response<FoldersReturn>
+                        ) {
+
+                            if (response.isSuccessful) {
+                                var arrAllReports: Array<Folders?> =
+                                    arrayOfNulls<Folders>(response.body()!!.data.size)
+
+                                for ((index, item) in response.body()!!.data.withIndex()) {
+                                    arrAllReports[index] = item
+                                }
+
+                                var allReports: List<Folders?> = arrAllReports.asList()
+
+                                allPastasLiveData.value = allReports
+
+
+                                allPastasLiveData.observe(requireActivity()) { reports ->
+                                    reports.let { adapter.submitList(it) }
+                                }
+
+                            } else {
+
+                            }
+                        }
+
+                        override fun onFailure(call: Call<FoldersReturn>, t: Throwable) {
+                            Toast.makeText(activity, "DEU ERRO", Toast.LENGTH_LONG).show()
+                        }
+                    })*/
+
+
+                }.toList()
+
+            }
+
+
+        }
+        val itemTouchHelperEdit = ItemTouchHelper(swipeHandlerEdit)
+        itemTouchHelperEdit.attachToRecyclerView(recyclerView)
 
         return root
     }
