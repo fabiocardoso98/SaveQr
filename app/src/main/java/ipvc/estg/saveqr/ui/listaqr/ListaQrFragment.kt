96c9c1f85f5c2bcf31bbdc6b75c36b3a58b026f4
joaoboas98 <62546037+joaoboas98@users.noreply.github.com>
2021-07-13 12:06:42 +0100

    registar

--- app/src/main/java/ipvc/estg/saveqr/ui/listaqr/ListaQrFragment.kt
+++ app/src/main/java/ipvc/estg/saveqr/ui/listaqr/ListaQrFragment.kt
@@ -9,15 +9,19 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ipvc.estg.saveqr.R
 import ipvc.estg.saveqr.Swipes.SwipeToDeleteCallback
+import ipvc.estg.saveqr.Swipes.SwipeToEditCallback
 import ipvc.estg.saveqr.api.ServiceBuilder
 import ipvc.estg.saveqr.api.api.endpoints.QrCodesEndpoint
 import ipvc.estg.saveqr.api.api.models.Folders
@@ -129,6 +133,63 @@ class ListaQrFragment : Fragment() {
                 })
             }
         }
+
+
+        val swipeHandlerEdit = object : SwipeToEditCallback(requireContext()) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position: Int = viewHolder.adapterPosition
+                val id: Int = allReportsLiveData.value?.get(position)?.id ?: 0
+                val pastaTemp: Qrcodes? = allReportsLiveData.value!![position]
+                val bundle = bundleOf("id" to pastaTemp!!.id,"name" to pastaTemp!!.name, "latlng" to pastaTemp!!.latlng, "tipoId" to pastaTemp!!.categoryId )
+                findNavController().navigate(R.id.nav_addQrUpdate,bundle)
+                allReportsLiveData.value = allReportsLiveData.value!!.toMutableList().apply {
+                    removeAt(position)
+
+                    /* val call = request.getPastaByUser(idUser)
+                     call.enqueue(object : Callback<FoldersReturn> {
+                         override fun onResponse(
+                             call: Call<FoldersReturn>,
+                             response: Response<FoldersReturn>
+                         ) {
+
+                             if (response.isSuccessful) {
+                                 var arrAllReports: Array<Folders?> =
+                                     arrayOfNulls<Folders>(response.body()!!.data.size)
+
+                                 for ((index, item) in response.body()!!.data.withIndex()) {
+                                     arrAllReports[index] = item
+                                 }
+
+                                 var allReports: List<Folders?> = arrAllReports.asList()
+
+                                 allPastasLiveData.value = allReports
+
+
+                                 allPastasLiveData.observe(requireActivity()) { reports ->
+                                     reports.let { adapter.submitList(it) }
+                                 }
+
+                             } else {
+
+                             }
+                         }
+
+                         override fun onFailure(call: Call<FoldersReturn>, t: Throwable) {
+                             Toast.makeText(activity, "DEU ERRO", Toast.LENGTH_LONG).show()
+                         }
+                     })*/
+
+
+                }.toList()
+
+            }
+
+
+        }
+        val itemTouchHelperEdit = ItemTouchHelper(swipeHandlerEdit)
+        itemTouchHelperEdit.attachToRecyclerView(recyclerView)
+
+
         val itemTouchHelper = ItemTouchHelper(swipeHandler)
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
