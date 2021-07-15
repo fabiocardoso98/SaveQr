d6c57c00b2d77a5b3c41052820bfc58bcc96ccf2
SergioRCarvalho <sergio_pvz3@hotmail.com>
2021-07-15 01:03:08 +0100

    atualizacao
    lerqr apartir do slpash screen e da pasta a funcionar, falta verificar login

--- app/src/main/java/ipvc/estg/saveqr/ui/addqrupdate/AddQrUpdate.kt
+++ app/src/main/java/ipvc/estg/saveqr/ui/addqrupdate/AddQrUpdate.kt
@@ -1,14 +1,16 @@
 package ipvc.estg.saveqr.ui.addqrupdate
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 import ipvc.estg.saveqr.R
-import ipvc.estg.saveqr.ui.addqrupdate.AddQrUpdateViewModel
+import ipvc.estg.saveqr.ui.lerQr.LerQrActivity
 
 class AddQrUpdate : Fragment() {
 //pena
@@ -24,7 +26,14 @@ class AddQrUpdate : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_qr_update_fragment, container, false)
+        val root = inflater.inflate(R.layout.add_qr_update_fragment, container, false)
+        val addqr: ImageView = root.findViewById(R.id.plus)
+        addqr.setOnClickListener {
+            val intent = Intent(requireContext(), LerQrActivity::class.java)
+            intent.putExtra("EXTRA", "")
+            startActivity(intent)
+        }
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -32,5 +41,6 @@ class AddQrUpdate : Fragment() {
         viewModel = ViewModelProviders.of(this).get(AddQrUpdateViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
 
 }
